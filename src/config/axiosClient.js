3b83adebf8fbfc113289a7c90fd9ee6de75bcafe
TrinidadCarlos <carlostrinidad952@gmail.com>
2022-01-09 13:24:05 -0600

    se agrega la ruta de heroku

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-// const BASEPATH = process.env.BACKEND_BARBERSHOP || 'http://localhost:9000/barbershop/api/';
+const BASE_PATH = process.env.PUBLIC_URL|| 'http://localhost:9000/barbershop/api/';
 
 
 const axiosClient = axios.create({
-   baseURL: 'https://desolate-sea-20956.herokuapp.com/barbershop/api/'
+   // baseURL: 'https://barber-shop-mern.herokuapp.com/barbershop/api'
+   baseURL: BASE_PATH
 });
 
 export default axiosClient;
