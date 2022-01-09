e5fedf6dc2c0813a50d6f4fa874587dd11250066
TrinidadCarlos <carlostrinidad952@gmail.com>
2022-01-09 13:34:03 -0600

    se agrega la ruta de heroku

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -5,6 +5,7 @@ const BASE_PATH = process.env.PUBLIC_URL|| 'http://localhost:9000/barbershop/api
 const axiosClient = axios.create({
    // baseURL: 'https://barber-shop-mern.herokuapp.com/barbershop/api'
    baseURL: BASE_PATH
+   
 });
 
 export default axiosClient;
